a3220661fc4e08bd238c927417a3998c908d6d10
Angelillo15 <contact@angelillo15.es>
2023-06-21 20:53:17 +0200

    NuxtJS Structure

---
+++ i18n.config.ts
@@ -0,0 +1,10 @@
+export default defineI18nConfig(() => ({
+  legacy: false,
+  locale: "es",
+  defaultLocale: "es",
+  messages: {
+    es: {
+      welcome: "Bienvenido a Minearte",
+    },
+  },
+}));
