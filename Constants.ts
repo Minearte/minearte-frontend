b7a92dcdc3cae5575762588066582234eba913c7
Angelillo15 <contact@angelillo15.es>
2023-06-23 15:32:41 +0200

    Added constants

---
+++ Constants.ts
@@ -0,0 +1,6 @@
+export default {
+    API_URL_DEV: 'http://localhost:8080',
+    API_URL_PROD: 'https://api.minearte.net',
+    DISCORD_GUILD_ID: '835903761058234368',
+    SERVER_IP: 'mc.minearte.net',
+}
