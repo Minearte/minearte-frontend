ca3522b5f7580eeb857d27eede33eba19ce00079
Angelillo15 <contact@angelillo15.es>
2023-06-23 15:40:56 +0200

    Added discord url

--- Constants.ts
+++ Constants.ts
@@ -2,5 +2,6 @@ export default {
     API_URL_DEV: 'http://localhost:8080',
     API_URL_PROD: 'https://api.minearte.net',
     DISCORD_GUILD_ID: '835903761058234368',
+    DISCORD_INVITE_URL: 'https://discord.minearte.net',
     SERVER_IP: 'mc.minearte.net',
 }
