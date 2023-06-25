e61d561fe795de390d068b116e98f675c416d229
Angelillo15 <contact@angelillo15.es>
2023-06-25 21:52:32 +0200

    Store layout

---
+++ layouts/tienda.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="xl:mx-52 lg:mx-32 md:mx-16 sm:mx-10 mx-4">
+    <div class=" w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
+        <li class="mr-2">
+          <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="true" class="inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Home</button>
+        </li>
+        <li class="mr-2">
+          <button id="services-tab" data-tabs-target="#services" type="button" role="tab" aria-controls="services" aria-selected="false" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300">Rangos Survival</button>
+        </li>
+        <li class="mr-2">
+          <button id="statistics-tab" data-tabs-target="#statistics" type="button" role="tab" aria-controls="statistics" aria-selected="false" class="inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300">Rangos BoxPVP</button>
+        </li>
+      </ul>
+      <div id="defaultTabContent">
+        <slot />
+      </div>
+    </div>
+
+  </div>
+</template>
+
