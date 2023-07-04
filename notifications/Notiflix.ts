01c98029cfed548c4bed458201785ac40cf9ea2a
Angelillo15 <contact@angelillo15.es>
2023-07-04 12:27:12 +0200

    Added noytiflix instance

---
+++ notifications/Notiflix.ts
@@ -0,0 +1,39 @@
+import Notiflix from "notiflix";
+function Init() {
+    Notiflix.Confirm.init({
+        backgroundColor: "#020910",
+        borderRadius: "5px",
+        titleColor: "#fff",
+        messageColor: "#000000",
+        okButtonBackground: "#fc764e",
+        cancelButtonBackground: "#fc683f",
+    })
+
+    Notiflix.Report.init({
+        backgroundColor: "#020910",
+        borderRadius: "5px",
+        success: {
+            titleColor: "#fff",
+            messageColor: "#fff",
+            buttonBackground: "#fc764e",
+            buttonColor: "#fff",
+        },
+        failure: {
+            titleColor: "#fff",
+            messageColor: "#fff",
+            buttonBackground: "#fc764e",
+            buttonColor: "#fff",
+        }
+    });
+}
+
+const Confirm = Notiflix.Confirm
+const Notify = Notiflix.Notify
+const Report = Notiflix.Report
+
+export {
+    Confirm,
+    Notify,
+    Report,
+    Init
+}
